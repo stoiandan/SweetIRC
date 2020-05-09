145d715af972f96aeab4e667acd91fd969524eeb
Dan Stoian <danmihaistoian@gmail.com>
2020-05-09 23:29:41 +0300

    Initial Commit

---
+++ SweetIRC/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SweetIRC
+//
+//  Created by Dan Stoian on 09/05/2020.
+//  Copyright © 2020 Dan Stoian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
