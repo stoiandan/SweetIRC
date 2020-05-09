34a8735718b6731e91096b40fd1c526311769f4f
Dan Stoian <danmihaistoian@gmail.com>
2020-05-10 00:09:35 +0300

    Initial code commit

--- SweetIRC/ContentView.swift
+++ SweetIRC/ContentView.swift
@@ -9,9 +9,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var nickName : String = ""
+    @State var userName : String = ""
+    @State var realName : String = ""
+    
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack {
+            HStack {
+                Text("Nickname: ")
+                TextField("Please enter your nickname...", text: $nickName).frame(width: 250.0)
+                Spacer()
+            }
+            HStack {
+                Text("Username: ")
+                TextField("Please enter your username...", text: $userName).frame(width: 250.0)
+                Spacer()
+                
+            }
+            HStack {
+                Text("Real name: ")
+                TextField("Please enter your real name...", text: $realName).frame(width: 250.0)
+                Spacer()
+                
+            }
+            List {
+                Text("Servers: ").font(.headline)
+                ForEach(0..<10, id: \.self){ index in
+                    Text("\(index)")
+                }
+            }.listStyle(SidebarListStyle())
+            Button(action: {
+                
+            }){
+                Text("Connect")
+            }.padding(.bottom)
+        }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
     }
 }
 
