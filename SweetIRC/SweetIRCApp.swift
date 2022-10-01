c893ae895623bda95d98cfe4406a7644e1494626
Dan Stoian <danmihaistoian@gmail.com>
2022-10-01 22:39:56 +0300

    Initial Commit

---
+++ SweetIRC/SweetIRCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetIRCApp.swift
+//  SweetIRC
+//
+//  Created by Dan Stoian on 01.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SweetIRCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
